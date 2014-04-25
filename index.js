cedc6dc7214312c0371fd26390b02cb6baf9d0d0
David Banham <david@banham.id.au>
2014-04-25 22:25:06 +1000

    Add wrapper

---
+++ index.js
@@ -0,0 +1,8 @@
+// Include the CoffeeScript interpreter so that .coffee files will work
+var coffee = require('coffee-script');
+ 
+// Explicitly register the compiler if required. This became necessary in CS 1.7
+if (typeof coffee.register !== 'undefined') coffee.register();
+ 
+// Include our application file
+var app = require(process.argv[2] || './app.coffee');
